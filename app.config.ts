408124d3dd27782eb38b7083c193017229289fd7
Luke <errooo@gmail.com>
2024-10-25 22:12:54 +0800

    1 change by Nuxt Studio

--- app.config.ts
+++ app.config.ts
@@ -1,26 +1,13 @@
 export default defineAppConfig({
   // Configure Content Wind here
   cover: '/cover.jpg',
-
   socials: {
     twitter: 'Atinux',
     github: 'Atinux/content-wind'
   },
-
   prose: {
     h1: {
       icon: 'carbon:sailboat-coastal'
-    },
-
-    copyButton: {
-      iconCopy: 'iconoir:3d-arc-center-pt',
-      iconCopied: 'teenyicons:360-solid'
-    }
-  },
-
-  nuxtIcon: {
-    aliases: {
-      light-mode: 'bx:arrow-back'
     }
   }
 })
